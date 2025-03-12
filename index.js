431efda149709365703911b699210595e6a4eadc
Marvin Mango <marvinke2001@gmail.com>
2025-03-12 22:08:21 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// 1. Multiply num1 and num2 to get 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+// 2. Generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1;
+
+// 3. Calculate the remainder of dividing num3 by num4 to get 4
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+// 4. Find the highest number in a set (should return 20)
+let max = Math.max(10, 15, 20, 5);
